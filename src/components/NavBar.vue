6bfac1d446819770034cbd0b8131b02513994538
BertaVR <bverges@cifpfbmoll.eu>
2022-02-23 17:37:57 +0100

    feat: añadido navBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Organic Garden
+    <img src='../assets/vegetable.png' class="logo-navbar">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <router-link class="nav-item nav-link" exact-active-class="active" to='home'> Home</router-link>
+      <router-link  class="nav-item nav-link" to='about'> About</router-link>
+
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+
+</script>
+<style>
+.logo-navbar{
+  width: 35px ;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
+
   <div class="home">
+    <NavBar/>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,11 +10,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import NavBar from "@/components/NavBar.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-  },
+    NavBar
+},
 };
 </script>
