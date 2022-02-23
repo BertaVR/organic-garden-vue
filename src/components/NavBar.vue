e56cc7d7610ed8d9b4e172551380382df245e665
BertaVR <bverges@cifpfbmoll.eu>
2022-02-23 19:21:29 +0100

    feat: navBar, ffalta que funcione el dropdown menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,29 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Organic Garden
-    <img src='../assets/vegetable.png' class="logo-navbar">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg navbar-dark lila fixed">
+    <a class="navbar-brand" href="#">
+      Organic Garden
+      <img src="../assets/vegetable.png" class="logo-navbar" />
+    </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <router-link class="nav-item nav-link" exact-active-class="active" to='home'> Home</router-link>
-      <router-link  class="nav-item nav-link" to='about'> About</router-link>
-
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link class="nav-item nav-link" exact-active-class="active" to="/">Home</router-link>
+        <router-link class="nav-item nav-link" exact-active-class="active" to="about">About</router-link>
+      </div>
     </div>
-  </div>
-</nav>
+    
+  </nav>
 </template>
 <script>
 
 </script>
 <style>
-.logo-navbar{
-  width: 35px ;
+.logo-navbar {
+  width: 35px;
+}
+.lila {
+  background-color: #4d3863;
 }
 </style>
