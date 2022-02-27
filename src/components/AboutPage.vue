a74bb3bbf0cde2a96f8c95743418c256f38b8444
BertaVR <bverges@cifpfbmoll.eu>
2022-02-27 23:36:08 +0100

    about for desktop

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,153 @@
+<template>
+    <div class="team">
+        <div class="person odd">
+            
+            <img src="../assets/capy.jpg" alt="A capybara, the foundator" class="img-thumbnail" />
+            <div>
+                <h3>The foundator</h3>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                    consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+                    quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                    nulla pariatur?
+                </p>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                    consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+                    quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                    nulla pariatur?
+                </p>
+            </div>
+        </div>
+        <hr />
+
+        <div class="person even">
+            <img src="../assets/caracal.jpg" alt="A caracal, the president" class="img-thumbnail" />
+
+            <div>
+                <h3>The President</h3>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                    consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+                    quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                    nulla pariatur?
+                </p>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                    consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+                    quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                    nulla pariatur?
+                </p>
+            </div>
+        </div>
+        <hr />
+
+        <div class="person odd pig">
+            <img src="../assets/pig.jpg" alt="A pig, the vice-president" class="img-thumbnail" />
+
+            <div>
+                <h3>The vice-president</h3>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                    consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+                    quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                    nulla pariatur?
+                </p>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                    consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                    exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+                    quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                    nulla pariatur?
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.team {
+    margin: 40px;
+    display: grid;
+    grid-template-rows: 1fr 40px 1fr 40px 1fr;
+    grid-template-columns: 1fr;
+}
+.person {
+    display: flex;
+    align-items: flex-start;
+}
+.person p{
+        text-align: justify;
+        margin: 40px;
+
+}
+.even {
+    flex-direction: row-reverse;
+}
+.person img {
+    float: left;
+    width: 400px;
+    height: 400px;
+    object-fit: cover;
+    margin-top: 40px;
+}
+@media (min-width: 1280px) {
+  .person img {
+  filter: saturate(0.5);}
+
+    
+}
+.person img:hover {
+    filter: grayscale(0%);
+    filter: contrast(1.25);
+    filter: saturate(1);
+
+}
+</style>
