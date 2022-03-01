196207a731e030a6037e2ecb6741f7008edd36a9
BertaVR <bverges@cifpfbmoll.eu>
2022-03-01 14:12:16 +0100

    MAde page title responsive

--- src/components/UpperHeader.vue
+++ src/components/UpperHeader.vue
@@ -2,10 +2,10 @@
     <!-- Navbar -->
     <a class="link-header" href="#">
         <div class="p-5 text-center bg-image page-header">
-            <h1>
+            <router-link class="centered" to="/">
                 <img class="mb-3 title" alt="Header, image of leaves" src="../assets/title.png" />
-                <h1 class="mb-3 bold">Join our organic farm</h1>
-            </h1>
+            </router-link>
+            <h1 class="mb-3 bold">Join our organic farm</h1>
         </div>
     </a>
 </template>
@@ -24,7 +24,9 @@ div.page-header {
     position: relative;
     color: #062717;
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-content: center;
     background-image: linear-gradient(
             rgba(255, 255, 255, 0.8),
             rgba(255, 255, 255, 0.98)
@@ -36,11 +38,18 @@ div.page-header {
     bottom: 0px;
     left: 0px;
 }
-
+@media (max-width: 470px) {
+    img.title {
+        width: 70vw;
+    }
+}
 .bold {
     font-weight: 700;
 }
 
+.centered {
+    align-self: center;
+}
 </style>
 <script>
 // @ is an alias to /src
